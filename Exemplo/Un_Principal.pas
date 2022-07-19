e6b5cd5d897c2f4378cbe6c528ebcfca25c64f6c
Luiz Henrique Luvizutto <31738097+luizluvizutto@users.noreply.github.com>
2022-07-19 11:46:28 -0300

    Update Un_Principal.pas

--- Exemplo/Un_Principal.pas
+++ Exemplo/Un_Principal.pas
@@ -38,26 +38,35 @@ implementation
 
 // *****************************************************************************
 procedure TForm1.FormCreate(Sender: TObject);
-var cPath: String;
 begin
    FMenuLateral := TmtMenuLateral.Create(Self);
-   // FMenuLateral.Log := Memo1.Lines;
-   cPath := ExtractFilePath( Application.ExeName );
-   FMenuLateral.PathImg := cPath + 'img\';
+   FMenuLateral.PathImg := ExtractFilePath( Application.ExeName ) + 'img\';
    FMenuLateral.Cor := $00F9E9DB;
-   FMenuLateral.Menus.CorNivel[1] := $00F0CAA8;
-   FMenuLateral.Menus.CorNivel[2] := $00E9AF7C;
+   FMenuLateral.Menus.CorNivel[1]  := $00E3E3FF;
+   FMenuLateral.Menus.CorNivel[2]  := $00DBFFED;
+   {
+   FMenuLateral.Menus.CorNivel[3]  := $00F0CAA8;
+   FMenuLateral.Menus.CorNivel[4]  := $00E9AF7C;
+   FMenuLateral.Menus.CorNivel[5]  := $00F0CAA8;
+   FMenuLateral.Menus.CorNivel[6]  := $00E9AF7C;
+   FMenuLateral.Menus.CorNivel[7]  := $00E9AF7C;
+   FMenuLateral.Menus.CorNivel[8]  := $00E9AF7C;
+   FMenuLateral.Menus.CorNivel[9]  := $00E9AF7C;
+   FMenuLateral.Menus.CorNivel[10] := $00E9AF7C;
+   }
 
-   FMenuLateral.Menus.Add('',           'MnProduto',       'Produtos',  NIL, 'produto.bmp');
-   FMenuLateral.Menus.Add('MnProduto',  'MnProdSetor',     'Setor',     NIL, 'Picture.bmp');
-   FMenuLateral.Menus.Add('MnProduto',  'MnProdSubSetor',  'Sub-Setor',  NIL, 'Picture.bmp');
+   FMenuLateral.Menus.Add('',              'MnProduto',       'Produtos',         NIL, 'produto.bmp');
+     FMenuLateral.Menus.Add('MnProduto',   'MnProdSetor',     'Setor',            NIL, 'Picture.bmp');
+     FMenuLateral.Menus.Add('MnProdSetor', 'MnProdSubSetor',  'Sub-Setor',        NIL, 'Picture.bmp');
 
-   FMenuLateral.Menus.Add('',           'MnPessoa'   ,     'Pessoas',   NIL, 'Pessoa.bmp');
-   FMenuLateral.Menus.Add('',           'MnNFe',           'Notas Fiscais', NIL, 'NF-e.bmp');
+   FMenuLateral.Menus.Add('',              'MnPessoa',        'Pessoas',          NIL, 'Pessoa.bmp');
+   FMenuLateral.Menus.Add('',              'MnNFe',           'Notas Fiscais',    NIL, 'NF-e.bmp');
 
-   FMenuLateral.Menus.Add('',           'MnFinanceiro',    'Financeiro', NIL, 'Financas.bmp');
-   FMenuLateral.Menus.Add('MnFinanceiro','MnFinanCPag',    'Contas a Pagar', NIL, 'CPagar.bmp');
-   FMenuLateral.Menus.Add('MnFinanceiro','MnFinanCRec',    'Contas a Receber', NIL, 'CReceber.bmp');
+   FMenuLateral.Menus.Add('',              'MnFinanceiro',    'Financeiro',       NIL, 'Financas.bmp');
+     FMenuLateral.Menus.Add('MnFinanceiro','MnFinanCPag',     'Contas a Pagar',   NIL, 'CPagar.bmp');
+     FMenuLateral.Menus.Add('MnFinanceiro','MnFinanCRec',     'Contas a Receber', NIL, 'CReceber.bmp');
+
+
    FMenuLateral.Ativo := true;
 end;
 // *****************************************************************************
